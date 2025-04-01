a87835d547eabf1ca4a75807b3352710c126faf8
Dan Denney <dd.subscriptions@gmail.com>
2025-04-01 08:03:19 -0500

    feat(lists): add long press for touch events

--- src/components/CostcoList.tsx
+++ src/components/CostcoList.tsx
@@ -11,8 +11,8 @@ export function CostcoList() {
       textColor="text-blue-800"
       accentColor="text-blue-600"
       iconColor="text-blue-600"
-      buttonGradientFrom="from-blue-600"
-      buttonGradientTo="to-blue-700"
+      buttonGradientFrom="from-red-600"
+      buttonGradientTo="to-red-700"
       buttonAccentColor="ring-blue-800"
     />
   )
