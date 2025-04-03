b908e26a165c22b867a0d755a89f80e50882e861
Dan Denney <dd.subscriptions@gmail.com>
2025-04-02 21:26:59 -0500

    feat(StoreSelector): balance the UI

--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -51,21 +51,20 @@ export function ShoppingList({
   const StoreSelector = ({ 
     value, 
     onChange, 
-    size = 'normal' 
+    className,
   }: { 
     value: 'Publix' | 'Costco' | 'Aldi', 
     onChange: (value: 'Publix' | 'Costco' | 'Aldi') => void,
-    size?: 'normal' | 'small'
+    className?: string
   }) => {
-    const isSmall = size === 'small'
     return (
-      <div className={`relative flex ${isSmall ? 'text-xs' : 'text-sm'} font-medium rounded-lg bg-gray-100 p-1`}>
+      <div className={`relative flex justify-end font-medium rounded-lg bg-gray-100 ${className}`}>
         {storeOptions.map((option) => (
           <button
             key={option.value}
             onClick={() => onChange(option.value)}
-            className={`relative z-10 flex-1 px-3 py-1.5 rounded-md transition-all ease-in-out ${
-              value === option.value ? 'bg-green-700 text-white' : 'text-gray-600 hover:text-gray-900'
+            className={`relative z-10 flex-1 px-3 py-1.5 rounded-md text-gray-400 transition-all ease-in-out hover:text-gray-700 ${
+              value === option.value ? 'bg-gray-200 text-gray-700' : ''
             }`}
           >
             {option.value}
@@ -377,16 +376,16 @@ export function ShoppingList({
                   >
                     Add
                   </button>
+                </div>
+                <div className="flex gap-2 justify-between">
+                  <StoreSelector className="grow" value={selectedStore} onChange={setSelectedStore} />
                   <button
                     type="button"
                     onClick={() => setIsModalOpen(false)}
-                    className="px-2 py-2 text-gray-600 hover:text-gray-800 focus:outline-none"
+                    className="px-5 py-2 text-gray-600 shrink-0 hover:text-gray-800 focus:outline-none"
                   >
                     <X className="w-5 h-5" />
                   </button>
-                </div>
-                <div className="flex gap-4">
-                  <StoreSelector value={selectedStore} onChange={setSelectedStore} />
                 </div>
               </form>
             </motion.div>
