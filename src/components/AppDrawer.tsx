fec3713cd332617a5c2511409c49351e4b3186aa
Dan Denney <dd.subscriptions@gmail.com>
2025-04-03 21:05:26 -0500

    feat(useclient): add use client to AppDrawer and TagInput

--- src/components/AppDrawer.tsx
+++ src/components/AppDrawer.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { MapPin, Menu, X, ShoppingBasket, Store, Link as LinkIcon } from 'lucide-react'
--- src/components/TagInput.tsx
+++ src/components/TagInput.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState, useRef, useEffect } from 'react'
 import { Tag as TagIcon, X, Check } from 'lucide-react'
 import { motion, AnimatePresence } from 'framer-motion'
